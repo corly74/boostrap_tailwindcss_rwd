97d3f4408cc607e9f1dad5554e51f03244619fb1
haleyhsieh <corlyyun@gmail.com>
2025-08-03 00:42:42 +0800

    新增cclouder專案

--- docs/CClouder/tailwind.config.js
+++ docs/CClouder/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // content: [],
-  content: ["./src/**/*.{html,jsx}"],
+  content: ["./docs/CClouder/**/*.{html,js,jsx}"],
   theme: {
     // theme裡面清除預設 ,再新增
     extend: {
